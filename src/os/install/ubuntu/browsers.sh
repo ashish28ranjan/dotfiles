8ec52ed187a6886d034de1844b47c71ebd23f87e
Ashish Ranjan <ashish28ranjan@protonmail.com>
2018-09-23 03:50:03 +0530

    [ubuntu][browsers] Removed nightly browsers; Added google-chrome-stable

--- src/os/install/ubuntu/browsers.sh
+++ src/os/install/ubuntu/browsers.sh
@@ -10,39 +10,20 @@ print_in_purple "\n   Browsers\n\n"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-if ! package_is_installed "google-chrome-unstable"; then
+if ! package_is_installed "google-chrome-stable"; then
 
     add_key "https://dl-ssl.google.com/linux/linux_signing_key.pub" \
-        || print_error "Chrome Canary (add key)"
+        || print_error "Google Chrome (add key)"
 
-    add_to_source_list "[arch=amd64] https://dl.google.com/linux/deb/ stable main" "google-chrome.list" \
-        || print_error "Chrome Canary (add to package resource list)"
+    add_to_source_list "[arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" "google-chrome.list" \
+        || print_error "Google Chrome (add to package resource list)"
 
     update &> /dev/null \
-        || print_error "Chrome Canary (resync package index files)"
+        || print_error "Google Chrome (resync package index files)"
 
 fi
 
-install_package "Chrome Canary" "google-chrome-unstable"
-install_package "Chromium" "chromium-browser"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
-
-if ! package_is_installed "firefox-trunk"; then
-
-    add_ppa "ubuntu-mozilla-daily/ppa" \
-        || print_error "Firefox Nightly (add PPA)"
-
-    update &> /dev/null \
-        || print_error "Firefox Nightly (resync package index files)"
-
-fi
-
-install_package "Firefox Nightly" "firefox-trunk"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+install_package "Google Chrome" "google-chrome-stable"
 
 printf "\n"
 
