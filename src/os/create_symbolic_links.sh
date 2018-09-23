67b6c83b6a030809edd6023c030fe46e8e5697c8
Ashish Ranjan <ashish28ranjan@protonmail.com>
2018-09-24 00:08:08 +0530

    [gpg] Symlinks to gpg.conf, gpg-agent.conf

--- src/os/create_symbolic_links.sh
+++ src/os/create_symbolic_links.sh
@@ -84,9 +84,77 @@ create_symlinks() {
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+create_full_path_symlinks() {
+
+    declare -a FILES_TO_SYMLINK=(
+
+        "gnupg/gpg.conf"
+        "gnupg/gpg-agent.conf"
+
+    )
+
+    # This will create a file $HOME/.gnupg/gpg.conf
+    # and symlink it to dotfiles/src/gnupg/gpg.conf
+
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+    local skipQuestions=false
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    skip_questions "$@" \
+        && skipQuestions=true
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    for i in "${FILES_TO_SYMLINK[@]}"; do
+
+        sourceFile="$(cd .. && pwd)/$i"
+        targetFile="$HOME/.$i"
+
+        # Create the full folder structure
+        mkdir -p "$(dirname $targetFile)"
+
+        if [ ! -e "$targetFile" ] || $skipQuestions; then
+
+            execute \
+                "ln -fs $sourceFile $targetFile" \
+                "$targetFile → $sourceFile"
+
+        elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+            print_success "$targetFile → $sourceFile"
+        else
+
+            if ! $skipQuestions; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+
+                    rm -rf "$targetFile"
+
+                    execute \
+                        "ln -fs $sourceFile $targetFile" \
+                        "$targetFile → $sourceFile"
+
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            fi
+
+        fi
+
+    done
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 main() {
     print_in_purple "\n • Create symbolic links\n\n"
     create_symlinks "$@"
+    create_full_path_symlinks "$@"
 }
 
 main "$@"
