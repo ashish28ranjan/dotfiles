c360bcb728301017267bfc9e5c1e1c30e3a4782e
Ashish Ranjan <ashish28ranjan@protonmail.com>
2018-09-23 18:38:37 +0530

    [git] Beautify the ask prompt when asking user details

--- src/os/create_local_config_files.sh
+++ src/os/create_local_config_files.sh
@@ -27,17 +27,17 @@ create_gitconfig_local() {
 
     if [ ! -e "$FILE_PATH" ] || [ -z "$FILE_PATH" ]; then
 
-        print_in_cyan "   Setting up Git local config...\n\n"
+        print_in_cyan "\n     Setting up Git local config...\n\n"
 
         defaultName=$( git config --global user.name )
         defaultEmail=$( git config --global user.email )
 
-        print_in_yellow "   (Press ENTER to continue with default, or type new value and then press ENTER)\n"
+        print_in_yellow "     (Press ENTER to continue with [default], or type a new value and then press ENTER)\n"
 
-        ask "Name:  [$defaultName] "
+        ask "  Name:  [$defaultName] "
         tmpName="$(get_answer)"
 
-        ask "Email: [$defaultEmail] "
+        ask "  Email: [$defaultEmail] "
         tmpEmail="$(get_answer)"
 
 
@@ -59,6 +59,7 @@ create_gitconfig_local() {
         >> "$FILE_PATH"
     fi
 
+    printf "\n"
     print_result $? "$FILE_PATH"
 
 }
