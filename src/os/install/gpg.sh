0c8a8ba6c270fd5e383fef42d18d6ab2def7a170
Ashish Ranjan <ashish28ranjan@protonmail.com>
2018-09-24 01:26:49 +0530

    [gpg] Restricted permissions of .gnupg/*

--- src/os/install/gpg.sh
+++ src/os/install/gpg.sh
@@ -13,8 +13,17 @@ main() {
 
     # CRITICAL: change permissions of the .gnupg folder and contents
     mkdir -p "$HOME/.gnupg"
+
+    # Own .gnupg folder
     chown -R $(whoami) "$HOME/.gnupg"
-    chmod 600 "$HOME/.gnupg/*"
+
+    # Make all files inside .gnupg rw-------
+    find "$HOME/.gnupg" -maxdepth 2 -type f -exec chmod 600 {} ';'
+
+    # Make all directories inside .gnupg rwx------
+    find "$HOME/.gnupg" -maxdepth 2 -type d -exec chmod 700 {} ';'
+
+    # Make .gnupg rwx------
     chmod 700 "$HOME/.gnupg"
 
     printf "\n"
