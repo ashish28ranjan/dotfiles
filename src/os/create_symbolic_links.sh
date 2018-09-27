0fe9c2c334cb38c9354127fdf1478bd203732744
Ashish Ranjan <ashish28ranjan@protonmail.com>
2018-09-27 14:50:51 +0530

    [symlink] Refactor code

--- src/os/create_symbolic_links.sh
+++ src/os/create_symbolic_links.sh
@@ -49,34 +49,7 @@ create_symlinks() {
         sourceFile="$(cd .. && pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
-        if [ ! -e "$targetFile" ] || $skipQuestions; then
-
-            execute \
-                "ln -fs $sourceFile $targetFile" \
-                "$targetFile → $sourceFile"
-
-        elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
-            print_success "$targetFile → $sourceFile"
-        else
-
-            if ! $skipQuestions; then
-
-                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-                if answer_is_yes; then
-
-                    rm -rf "$targetFile"
-
-                    execute \
-                        "ln -fs $sourceFile $targetFile" \
-                        "$targetFile → $sourceFile"
-
-                else
-                    print_error "$targetFile → $sourceFile"
-                fi
-
-            fi
-
-        fi
+        create_symbolick_link "$sourceFile" "$targetFile"
 
     done
 
@@ -116,34 +89,7 @@ create_full_path_symlinks() {
         # Create the full folder structure
         mkdir -p "$(dirname $targetFile)"
 
-        if [ ! -e "$targetFile" ] || $skipQuestions; then
-
-            execute \
-                "ln -fs $sourceFile $targetFile" \
-                "$targetFile → $sourceFile"
-
-        elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
-            print_success "$targetFile → $sourceFile"
-        else
-
-            if ! $skipQuestions; then
-
-                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-                if answer_is_yes; then
-
-                    rm -rf "$targetFile"
-
-                    execute \
-                        "ln -fs $sourceFile $targetFile" \
-                        "$targetFile → $sourceFile"
-
-                else
-                    print_error "$targetFile → $sourceFile"
-                fi
-
-            fi
-
-        fi
+        create_symbolick_link "$sourceFile" "$targetFile"
 
     done
 
