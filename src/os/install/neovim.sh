2a60e7b807017a520680c7121ffc7341e8ea423e
Ashish Ranjan <ashish28ranjan@protonmail.com>
2018-09-27 18:12:27 +0530

    [spacevim] Installer

--- src/os/install/neovim.sh
+++ src/os/install/neovim.sh
@@ -11,6 +11,15 @@ main() {
 
     "./$(get_os)/neovim.sh"
 
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Spacevim
+
+    execute \
+        "curl -sLf https://spacevim.org/install.sh | bash -s -- --install neovim \
+            > /dev/null 2>&1" \
+        "SpaceVim (installing only for neovim)"
+
     printf "\n"
 }
 
