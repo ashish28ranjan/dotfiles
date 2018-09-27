a75b24f772487fb60ba8358eac614d3e1d2183e7
Ashish Ranjan <ashish28ranjan@protonmail.com>
2018-09-27 18:37:17 +0530

    [tmuxifier] Update source repository before installing

--- src/os/install/tmuxifier.sh
+++ src/os/install/tmuxifier.sh
@@ -9,6 +9,12 @@ main() {
 
     print_in_purple "\n   Tmuxifier\n\n"
 
+    execute \
+        "git submodule update --recursive --remote > /dev/null 2>&1" \
+        "Tmuxifier source repository updated"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     local -r sourceFile="$(cd ../.. && pwd)/tmuxifier/bin/tmuxifier"
     local -r targetFile="$HOME/bin/tmuxifier"
 
