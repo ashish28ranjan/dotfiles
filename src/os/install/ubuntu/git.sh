3b9eb7ed85d09f6e58a671352f4c0b2a0bce6c05
Ashish Ranjan <ashish28ranjan@protonmail.com>
2018-10-18 01:49:33 +0530

    [git] Ubuntu ppa added

--- src/os/install/ubuntu/git.sh
+++ src/os/install/ubuntu/git.sh
@@ -8,4 +8,14 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Git\n\n"
 
+if ! package_is_installed "git"; then
+
+  add_ppa "git-core/ppa" \
+     || print_error "Git (add ppa)"
+
+fi
+
+update &> /dev/null \
+  || print_error "Git (resync package index files)"
+
 install_package "Git" "git"
