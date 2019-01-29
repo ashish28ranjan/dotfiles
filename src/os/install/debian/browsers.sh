45527c6b669a540f2fe0fce24ab4390fef402e1f
Ashish Ranjan <ashish28ranjan@protonmail.com>
2019-01-30 02:18:01 +0530

    [browser] Debian: Brave (stable) installer fixed

--- src/os/install/debian/browsers.sh
+++ src/os/install/debian/browsers.sh
@@ -12,10 +12,14 @@ print_in_purple "\n   Browsers\n\n"
 
 if ! package_is_installed "brave-browser"; then
 
+    os_codename="$(lsb_release -sc)"
+
     add_key "https://brave-browser-apt-release.s3.brave.com/brave-core.asc" \
         || print_error "Brave browser (add key)"
 
-    add_to_source_list "[arch=amd64] https://brave-browser-apt-release.s3.brave.com/ `lsb_release -sc` main" "brave-browser-release-`lsb_release -sc`.list" \
+    source /etc/os-release &> /dev/null
+
+    add_to_source_list "[arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $os_codename main" "brave-browser-release-$os_codename.list" \
         || print_error "Brave browser (add to package resource list)"
 
     update &> /dev/null \
@@ -23,7 +27,7 @@ if ! package_is_installed "brave-browser"; then
 
 fi
 
-install_package "Brave browser" "brave-browser brave-keyring"
+install_package "Brave (stable)" "brave-browser brave-keyring"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
