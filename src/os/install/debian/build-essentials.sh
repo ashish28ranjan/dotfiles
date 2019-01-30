4e95f6a0be4e7fec50b730e665fb26bd799eef68
Ashish Ranjan <ashish28ranjan@protonmail.com>
2019-01-30 23:00:28 +0530

    [os] Install apt-transport-https for Ubuntu & Debian

--- src/os/install/debian/build-essentials.sh
+++ src/os/install/debian/build-essentials.sh
@@ -8,6 +8,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Build Essentials\n\n"
 
+# Install tools for using https in apt sources
+install_package "apt-transport-https" "apt-transport-https"
+
 # Install tools for compiling/building software from source.
 install_package "Build Essential" "build-essential"
 
