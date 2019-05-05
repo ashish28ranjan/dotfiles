b22abba47d71723c0fc54e195b2628ac748c9e56
Ashish Ranjan <ashish28ranjan@protonmail.com>
2019-05-05 15:27:29 +0530

    [git-ydiff] Installer modified

--- src/os/install/macos/scripts.sh
+++ src/os/install/macos/scripts.sh
@@ -6,7 +6,27 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-print_in_purple "\n   Scripts\n\n"
+git-ydiff() {
 
-execute "create_symlink \"$HOME/bin/ydiff\" \"$HOME/bin/git-ydiff\"" \
-    "git-ydiff"
+    local targetFile="$HOME/bin/git-ydiff"
+    local sourceFile="$(which ydiff)"
+
+    if [ "$?" -eq 0 ]; then
+        execute "unlink $targetFile \
+                && create_symlink $sourceFile $targetFile" \
+            "git-ydiff"
+    else
+        print_error "ydiff not found"
+    fi
+
+}
+
+main() {
+
+    print_in_purple "\n   Scripts\n\n"
+
+    git-ydiff
+
+}
+
+main
